8090e118d3a8bad138fe02a1d6baf8e4cfb01d90
Administrator <gitlab_admin_7ebaf4@example.com>
2024-09-05 17:38:13 +0200

    upadte

---
+++ GatewayService/src/main/java/com/example/gatewayservice/utils/PortApi.java
@@ -0,0 +1,21 @@
+package com.example.gatewayservice.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class PortApi {
+
+//    @Value("${PORT_AUTH}")
+//    @Value("${portAuth:8085}")
+    public static String portAuth = "8085";
+//    @Value("${Port_USER")
+//    @Value("${portUser:8083}")
+    public static String portUser = "8083";
+//    @Value("${PORT_COMM}")
+//    @Value("${portComm:8084}")
+    public static String portComm = "8084" ;
+//    @Value("${PORT_CAR_RIDE}")
+//    @Value("${portCarRide:8082}")
+    public static String portCarRide = "8082";
+
+
+}
